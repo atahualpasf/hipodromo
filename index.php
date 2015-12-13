51e5f914622d6c4d1562261fe8828a5ed9573d64
Atahualpa Silva F <atahualpasilva@gmail.com>
2015-12-12 20:27:00 -0430

    Se agrego plugin videobackground y bootstrap-dialog. Todas las paginas .php

--- index.php
+++ index.php
@@ -29,6 +29,9 @@
     <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker-bs3.css">
     <!-- bootstrap wysihtml5 - text editor -->
     <link rel="stylesheet" href="plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+    <!-- Full responsive background video with css -->
+    <!-- <link rel="stylesheet" href="css/videobackground.css"> -->
+    <link rel="stylesheet" href="plugins/videobackground/videobackground.css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -38,245 +41,33 @@
     <![endif]-->
   </head>
   <body class="hold-transition skin-blue sidebar-mini">
-    <div class="wrapper">
-
-      <header class="main-header">
-        <!-- Logo -->
-        <a href="index.php" class="logo">
-          <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini">H<b>LR</b></span>
-          <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg">La <b>Rinconada</b></span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top" role="navigation">
-          <!-- Sidebar toggle button-->
-          <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-            <span class="sr-only">Toggle navigation</span>
-          </a>
-          <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-              <!-- User Account: style can be found in dropdown.less -->
-              <li class="dropdown user user-menu">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                  <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
-                  <span class="hidden-xs">Alexander Pierce</span>
-                </a>
-                <ul class="dropdown-menu">
-                  <!-- User image -->
-                  <li class="user-header">
-                    <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-                    <p>
-                      Alexander Pierce
-                      <small>Miembro desde Nov. 2012</small>
-                    </p>
-                  </li>
-                  <li class="user-footer">
-                    <div class="pull-right">
-                      <a href="#" class="btn btn-default btn-flat">Cerrar Sesión</a>
-                    </div>
-                  </li>
-                </ul>
-              </li>
-            </ul>
+    <div id="overlay">
+    </div>
+    <div id="bg-video"></div>
+    <div class="jumbotron vertical-center">
+      
+      <div class="container text-center">
+        <i class="flaticon-horse92"></i>
+        <h1>Hipódromo La Rinconada</h1>
+        <i class="flaticon-horse92 flip"></i>
+        <div class="row">
+          <div class="col-md-offset-4 col-md-4 col-md-offset-4">
+            <button class="btn bg-green btn-block btn-flat margin">INICIA SESIÓN</button>
           </div>
-        </nav>
-      </header>
-      <!-- Left side column. contains the logo and sidebar -->
-      <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-          <!-- Sidebar user panel -->
-          <div class="user-panel">
-            <div class="pull-left image">
-              <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-            </div>
-            <div class="pull-left info">
-              <p>Alexander Pierce</p>
-              <a href="#"><i class="fa fa-circle text-success"></i> En línea</a>
-            </div>
+          <div class="col-md-offset-4 col-md-4 col-md-offset-4">
+            <button class="btn bg-green btn-block btn-flat margin">REGISTRAR</button>
           </div>
-          <!-- search form -->
-          <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-              <input type="text" name="q" class="form-control" placeholder="Buscar...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-              </span>
-            </div>
-          </form>
-          <!-- /.search form -->
-          <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu">
-            <li class="header">Navegación Principal</li>
-            <li class="active treeview">
-              <a href="index.php">
-                <i class="fa fa-dashboard"></i> <span>Tablero</span>
-              </a>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-table"></i> <span>Tablas</span>
-                <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="pages/tables/apuestas.php"><i class="fa fa-circle-o"></i> Apuestas</a></li>
-                <li><a href="pages/tables/ejemplares.php"><i class="fa fa-circle-o"></i> Ejemplares</a></li>
-                <li><a href="pages/tables/entrenadores.php"><i class="fa fa-circle-o"></i> Entrenadores</a></li>
-                <li><a href="pages/tables/implementos.php"><i class="fa fa-circle-o"></i> Implementos</a></li>
-                <li><a href="pages/tables/jinetes.php"><i class="fa fa-circle-o"></i> Jinetes</a></li>
-                <li><a href="pages/tables/studs.php"><i class="fa fa-circle-o"></i> Studs</a></li>
-                <li><a href="pages/tables/usuarios.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
-              </ul>
-            </li>
-            </ul>
-        </section>
-        <!-- /.sidebar -->
-      </aside>
+        </div>
+      </div>
+      
+    </div>
+    
 
-      <!-- Content Wrapper. Contains page content -->
-      <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-          <h1>
-            Tablero
-            <small>Panel de Control</small>
-          </h1>
-          <ol class="breadcrumb">
-            <li><a href="index.php"><i class="fa fa-dashboard"></i> Inicio</a></li>
-            <li class="active">Tablero</li>
-          </ol>
-        </section>
-
-        <!-- Main content -->
-        <section class="content">
-          <!-- Small boxes (Stat box) -->
-          <div class="row">
-            <div class="col-lg-6 col-xs-6">
-              <!-- small box -->
-              <div class="small-box bg-aqua">
-                <div class="inner">
-                  <h3>150</h3>
-                  <p>Total ventas de Boletos</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-bag"></i>
-                </div>
-                <a href="#" class="small-box-footer">Más Información <i class="fa fa-arrow-circle-right"></i></a>
-              </div>
-            </div><!-- ./col -->
-            <div class="col-lg-6 col-xs-6">
-              <!-- small box -->
-              <div class="small-box bg-yellow">
-                <div class="inner">
-                  <h3>44</h3>
-                  <p>Usuarios Registrados</p>
-                </div>
-                <div class="icon">
-                  <i class="ion ion-person-add"></i>
-                </div>
-                <a href="#" class="small-box-footer">Más información <i class="fa fa-arrow-circle-right"></i></a>
-              </div>
-            </div><!-- ./col -->
-        </div><!-- /.row -->
-          <!-- Main row -->
-          <div class="row">
-            <!-- Left col -->
-            <section class="col-lg-7 connectedSortable">
-              <!-- Custom tabs (Charts with tabs)-->
-              <div class="nav-tabs-custom">
-                <!-- Tabs within a box -->
-                <ul class="nav nav-tabs pull-right">
-                  <li class="active"><a href="#revenue-chart" data-toggle="tab">Area</a></li>
-                  <li><a href="#sales-chart" data-toggle="tab">Donut</a></li>
-                  <li class="pull-left header"><i class="fa fa-inbox"></i> Sales</li>
-                </ul>
-                <div class="tab-content no-padding">
-                  <!-- Morris chart - Sales -->
-                  <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;"></div>
-                  <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;"></div>
-                </div>
-              </div><!-- /.nav-tabs-custom -->
-
-            </section><!-- /.Left col -->
-            <!-- right col (We are only adding the ID to make the widgets sortable)-->
-            <section class="col-lg-5 connectedSortable">
-
-              <!-- Calendar -->
-              <div class="box box-solid bg-green-gradient">
-                <div class="box-header">
-                  <i class="fa fa-calendar"></i>
-                  <h3 class="box-title">Calendar</h3>
-                  <!-- tools box -->
-                  <div class="pull-right box-tools">
-                    <!-- button with a dropdown -->
-                    <div class="btn-group">
-                      <button class="btn btn-success btn-sm dropdown-toggle" data-toggle="dropdown"><i class="fa fa-bars"></i></button>
-                      <ul class="dropdown-menu pull-right" role="menu">
-                        <li><a href="#">Add new event</a></li>
-                        <li><a href="#">Clear events</a></li>
-                        <li class="divider"></li>
-                        <li><a href="#">View calendar</a></li>
-                      </ul>
-                    </div>
-                    <button class="btn btn-success btn-sm" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                    <button class="btn btn-success btn-sm" data-widget="remove"><i class="fa fa-times"></i></button>
-                  </div><!-- /. tools -->
-                </div><!-- /.box-header -->
-                <div class="box-body no-padding">
-                  <!--The calendar -->
-                  <div id="calendar" style="width: 100%"></div>
-                </div><!-- /.box-body -->
-                <div class="box-footer text-black">
-                  <div class="row">
-                    <div class="col-sm-6">
-                      <!-- Progress bars -->
-                      <div class="clearfix">
-                        <span class="pull-left">Task #1</span>
-                        <small class="pull-right">90%</small>
-                      </div>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-green" style="width: 90%;"></div>
-                      </div>
-
-                      <div class="clearfix">
-                        <span class="pull-left">Task #2</span>
-                        <small class="pull-right">70%</small>
-                      </div>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-green" style="width: 70%;"></div>
-                      </div>
-                    </div><!-- /.col -->
-                    <div class="col-sm-6">
-                      <div class="clearfix">
-                        <span class="pull-left">Task #3</span>
-                        <small class="pull-right">60%</small>
-                      </div>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-green" style="width: 60%;"></div>
-                      </div>
-
-                      <div class="clearfix">
-                        <span class="pull-left">Task #4</span>
-                        <small class="pull-right">40%</small>
-                      </div>
-                      <div class="progress xs">
-                        <div class="progress-bar progress-bar-green" style="width: 40%;"></div>
-                      </div>
-                    </div><!-- /.col -->
-                  </div><!-- /.row -->
-                </div>
-              </div><!-- /.box -->
-
-            </section><!-- right col -->
-          </div><!-- /.row (main row) -->
-
-        </section><!-- /.content -->
-      </div><!-- /.content-wrapper -->
+    <!-- <div class="wrapper">
       <footer class="main-footer">
         <strong>Copyright &copy; 2014-2015 Hipodromo La Rinconada.</strong> Todos los derechos reservados.
       </footer>
-    </div><!-- ./wrapper -->
+    </div> -->
 
     <!-- jQuery 2.1.4 -->
     <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
@@ -288,6 +79,13 @@
     </script>
     <!-- Bootstrap 3.3.5 -->
     <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- Background video -->
+    <script src="plugins/videobackground/videobackground.js"></script>
+    <script type="text/javascript">
+      $(document).ready(function() {
+        $('#bg-video').videoBackground('videos/racehorseslowmotion-hd.mp4');
+      });
+    </script>
     <!-- Morris.js charts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
     <script src="plugins/morris/morris.min.js"></script>
---
+++ plugins/videobackground/videobackground.js
@@ -0,0 +1,35 @@
+(function ($) {
+    //add the videBackground function to the jQuery library
+    $.fn.videoBackground = function(video, options) {
+        var that = this;
+        if(typeof options === 'undefined') options = {};
+
+        //check and see if options were passed
+        var settings = {};
+        settings.autoplay = typeof options.autoplay !== 'undefined' ? options.autoplay : 'autoplay';
+        settings.muted = typeof options.muted !== 'undefined' ? options.muted : false;
+        settings.loop = typeof options.loop !== 'undefined' ? options.loop : 'loop';
+        settings.fit = typeof options.fit !== 'undefined' ? options.fit : 'fill';
+        settings.src = typeof options.src !== 'undefined' ? options.src : video;
+
+        //append a video tag to the target element
+        that.append($('<video>')
+            .prop({'autoplay': settings.autoplay, 'muted': settings.muted, 'loop': settings.loop, 'src': settings.src})
+            .css({'height': '100%', 'width': '100%', 'object-fit': settings.fit, 'overflow': 'hidden', 'position': 'absolute'}));
+
+        //sets the initial video size
+        resizeVideo(that);
+
+        //updates the sizing as the window size changes
+        $(window).on('resize', function(e) {
+            that.css({'width': '100%', 'height': '100%'});
+            resizeVideo(that);
+        });
+
+        //function to adjust the video size as needed
+        function resizeVideo(vidElement) {
+            vidElement.height(vidElement.height());
+            vidElement.width(vidElement.width())
+        }
+    }
+}(jQuery));
