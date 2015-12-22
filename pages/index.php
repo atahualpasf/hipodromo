8a1bf5d42052e36e8ce08b446cb380fd160cf920
Andrea Contreras <andre.contdi@gmail.com>
2015-12-22 11:27:27 -0430

    Se agrega el sidebar al modulo del header

--- pages/index.php
+++ pages/index.php
@@ -1,58 +1,6 @@
 <?php
   include($_SERVER['DOCUMENT_ROOT'] . 'hipodromo/includes/header.inc.php');
 ?>
-      <!-- Left side column. contains the logo and sidebar -->
-      <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-          <!-- Sidebar user panel -->
-          <div class="user-panel">
-            <div class="pull-left image">
-              <img src="<?php echo $db->getRootUri() . 'dist/img/user2-160x160.jpg'; ?>" class="img-circle" alt="User Image">
-            </div>
-            <div class="pull-left info">
-              <p>Alexander Pierce</p>
-              <a href="#"><i class="fa fa-circle text-success"></i> En línea</a>
-            </div>
-          </div>
-          <!-- search form -->
-          <form action="#" method="get" class="sidebar-form">
-            <div class="input-group">
-              <input type="text" name="q" class="form-control" placeholder="Buscar...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i></button>
-              </span>
-            </div>
-          </form>
-          <!-- /.search form -->
-          <!-- sidebar menu: : style can be found in sidebar.less -->
-          <ul class="sidebar-menu">
-            <li class="header">Navegación Principal</li>
-            <li class="active treeview">
-              <a href="index.php">
-                <i class="fa fa-dashboard"></i> <span>Tablero</span>
-              </a>
-            </li>
-            <li class="treeview">
-              <a href="#">
-                <i class="fa fa-table"></i> <span>Tablas</span>
-                <i class="fa fa-angle-left pull-right"></i>
-              </a>
-              <ul class="treeview-menu">
-                <li><a href="apuestas.php"><i class="fa fa-circle-o"></i> Apuestas</a></li>
-                <li><a href="ejemplares.php"><i class="fa fa-circle-o"></i> Ejemplares</a></li>
-                <li><a href="entrenadores.php"><i class="fa fa-circle-o"></i> Entrenadores</a></li>
-                <li><a href="implementos.php"><i class="fa fa-circle-o"></i> Implementos</a></li>
-                <li><a href="jinetes.php"><i class="fa fa-circle-o"></i> Jinetes</a></li>
-                <li><a href="studs.php"><i class="fa fa-circle-o"></i> Studs</a></li>
-                <li><a href="usuarios.php"><i class="fa fa-circle-o"></i> Usuarios</a></li>
-              </ul>
-            </li>
-            </ul>
-        </section>
-        <!-- /.sidebar -->
-      </aside>
-
       <!-- Content Wrapper. Contains page content -->
       <div class="content-wrapper">
         <!-- Content Header (Page header) -->
