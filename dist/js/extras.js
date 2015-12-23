8aa86481b116ef484deb703d5dc2f4215bfc2e88
Atahualpa Silva F <atahualpasilva@gmail.com>
2015-12-23 18:23:12 -0430

    Archivo que envia la data del cierre de sesión al revidor

---
+++ dist/js/extras.js
@@ -0,0 +1,27 @@
+function cerrarSesion(btn) {
+  var data = 'logout='+$(btn).val();
+  $.ajax({
+      url: _INCL_ROOT + 'user-connection.inc.php',
+      type: 'POST',
+      data: data,
+      success: function (data) {
+        var dataResponse = JSON.parse(data);
+        console.log(dataResponse.response.data);
+        if (dataResponse.action.action === 'success') {
+          window.location.replace(_ROOT);
+        }
+      },
+      error: function(data) {
+        console.log(data);
+        var dataResponse = JSON.parse(data.responseText);
+        console.log(dataResponse);
+      },
+      cache: false,
+  });
+}
+$(document).ready(function() {
+  $('#btn-cerrarsesion').click(function(event) {
+    event.preventDefault();
+    cerrarSesion(this);
+  });
+});
