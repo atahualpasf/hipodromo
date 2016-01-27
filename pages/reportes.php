054021e72d415240a3a5bf6de17682cf703e27a4
Andrea Contreras <andre.contdi@gmail.com>
2016-01-27 15:46:13 -0430

    Se agregó: Reporte #27

--- pages/reportes.php
+++ pages/reportes.php
@@ -379,6 +379,20 @@
               </div>
             </div><!-- ./col -->
             
+            <div class="col-lg-12 col-xs-12">
+              <!-- small box -->
+              <div class="small-box bg-aqua">
+                <div class="inner">
+                  <h3>#27</h3>
+                  <p>El promedio de los ejemplares que corrieron en las ultimas 50 carreras según su pelaje.</p>
+                </div>
+                <div class="icon">
+                  <i class="ion ion-document-text"></i>
+                </div>
+                <a href="../reports/reports.php?report=R27ElPromedioDeEjemplaresQueCorrieronEnLasUltimas50" class="small-box-footer">Ver reporte <i class="fa fa-arrow-circle-right"></i></a>
+              </div>
+            </div><!-- ./col -->
+            
           </div><!-- /.row -->
 
         </section><!-- /.content -->
